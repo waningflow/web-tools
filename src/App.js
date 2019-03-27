ea866e8e46e3190704318b5e00ffd5ffe5d6a5f5
waningflow <>
2019-03-27 15:35:06 +0800

    update eslintConfig

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/pages/home/Home.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+    constructor () {
+        
+    }
+
+    render () {
+        if(true){}
+        return (
+            <div >
+                
+            </div>
+        )
+    }
+}
+
+export default Home
