1ec1a480afdcca1c1aaab4c6a3b9480211dc8d98
waningflow <>
2019-04-03 23:57:34 +0800

    update logo generate

--- src/pages/LogoGenerate/LogoGenerate.js
+++ src/pages/LogoGenerate/LogoGenerate.js
@@ -5,6 +5,7 @@ import { withStyles } from '@material-ui/core/styles'
 import ColorPicker from './items/ColorPicker'
 import Typography from '@material-ui/core/Typography'
 import GridBox from './items/GridBox'
+import FontPicker from 'font-picker-react'
 
 type Props = {
   classes: Object
@@ -18,13 +19,15 @@ type State = {
   highlightText: string,
   fontSize: number,
   fontFamily: string,
-  firstText: string,
-  lastText: string
+  // firstText: string,
+  // lastText: string
 }
+
+const api_key = 'AIzaSyD5hekAz930QwvNrQe6orJBFL0TO5iH0_k'
 
 const styles = theme => ({
   logoGenerateHandBar: {
-    padding: '20px 0',
+    padding: '20px 5px',
     borderRadius: '5px',
     border: '2px dashed',
     borderColor: theme.palette.secondary.main
@@ -43,7 +46,8 @@ const styles = theme => ({
     flex: 1,
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    minWidth: 0
   },
   logoGeneratePreviewContainer: {
     height: '600px',
@@ -69,8 +73,19 @@ const styles = theme => ({
   },
   logoGenerateLogoContainer: {
     padding: '20px',
-    border: '2px solid',
-    borderColor: theme.palette.secondary.main
+    // border: '2px solid',
+    borderColor: theme.palette.secondary.main,
+    fontSize: '60px',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    letterSpacing: '1px'
+  },
+  logoGenerateText: {
+    display: 'flex'
+    // border: '1px solid',
+    // borderColor: theme.palette.primary.main,
   }
 })
 
@@ -94,20 +109,21 @@ const ColorNameList = [
   }
 ]
 
+const firstText = 'You'
+const lastText = 'Tube'
+
 class LogoGenerate extends Component<Props, State> {
   constructor(props: Props) {
     super(props)
 
     this.state = {
-      logoBackgroundColor: { r: 244, g: 0, b: 0, a: 1 },
-      highlightBackgroundColor: { r: 255, g: 255, b: 255, a: 0 },
-      firstTextColor: { r: 255, g: 255, b: 255, a: 0 },
-      lastTextColor: { r: 255, g: 255, b: 255, a: 0 },
+      logoBackgroundColor: { r: 255, g: 0, b: 0, a: 1 },
+      highlightBackgroundColor: { r: 255, g: 255, b: 255, a: 1 },
+      firstTextColor: { r: 255, g: 255, b: 255, a: 1 },
+      lastTextColor: { r: 255, g: 0, b: 0, a: 1 },
       highlightText: 'last',
       fontSize: 60,
-      fontFamily: '',
-      firstText: 'You',
-      lastText: 'Tube'
+      fontFamily: 'Oswald', // or Fira Sans Condensed
     }
   }
 
@@ -119,13 +135,55 @@ class LogoGenerate extends Component<Props, State> {
 
   render() {
     const { classes } = this.props
+    const {
+      fontFamily,
+      logoBackgroundColor,
+      highlightBackgroundColor,
+      firstTextColor,
+      lastTextColor
+    } = this.state
+    const parseColor = color =>
+      `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`
     return (
       <Grid container spacing={24} style={{ marginTop: '20px' }}>
         <Grid item xs={12} md={8} xl={8}>
           <div className={classes.logoGeneratePreviewContainer}>
             <GridBox gridNum={51} />
             <div className={classes.logoGenerateLogoFullContainer}>
-              <div className={classes.logoGenerateLogoContainer} />
+              <div
+                className={[
+                  classes.logoGenerateLogoContainer,
+                  'apply-font'
+                ].join(' ')}
+                style={{
+                  backgroundColor: parseColor(logoBackgroundColor)
+                }}
+              >
+                <div
+                  className={classes.logoGenerateText}
+                  style={{
+                    padding: '0px 5px',
+                    color: parseColor(firstTextColor)
+                  }}
+                  contentEditable={true}
+                  spellCheck={false}
+                >
+                  {firstText}
+                </div>
+                <div
+                  className={classes.logoGenerateText}
+                  style={{
+                    padding: '0px 5px',
+                    borderRadius: '10px',
+                    backgroundColor: parseColor(highlightBackgroundColor),
+                    color: parseColor(lastTextColor)
+                  }}
+                  contentEditable={true}
+                  spellCheck={false}
+                >
+                  {lastText}
+                </div>
+              </div>
             </div>
           </div>
         </Grid>
@@ -146,6 +204,25 @@ class LogoGenerate extends Component<Props, State> {
                 </div>
               )
             })}
+            <div className={classes.logoGenerateInputContainer}>
+              <div className={classes.logoGenerateLabel}>
+                <Typography variant="subtitle2">Font Family</Typography>
+              </div>
+              <div className={classes.logoGenerateInput}>
+                <FontPicker
+                  apiKey={api_key}
+                  activeFontFamily={fontFamily}
+                  limit={100}
+                  sort={'popularity'}
+                  variants={['700']}
+                  onChange={nextFont =>
+                    this.setState({
+                      fontFamily: nextFont.family
+                    })
+                  }
+                />
+              </div>
+            </div>
           </div>
         </Grid>
       </Grid>
