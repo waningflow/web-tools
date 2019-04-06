76e80e2a77ea9bc54bed8e1c166a2febb15eea48
waningflow <>
2019-04-06 15:53:16 +0800

    update home

---
+++ src/config/index.js
@@ -0,0 +1,20 @@
+export const MenuConfig = [
+  {
+    name: 'Home',
+    title: 'Home',
+    desc: 'Home page',
+    path: '/'
+  },
+  {
+    name: 'NpmDownload',
+    title: 'NPM Downloads',
+    desc: 'Downloads statistic of NPM packages',
+    path: '/npm-download'
+  },
+  {
+    name: 'LogoGenerate',
+    title: 'Logo Generate',
+    desc: 'Generate logo like YouTube',
+    path: '/logo-generate'
+  }
+]
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,55 @@
 import React, { Component } from 'react'
+import { MenuConfig } from '../../config'
+// import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import { withRouter } from 'react-router'
+import { withStyles } from '@material-ui/core/styles'
 
-export default class Home extends Component {
+const styles = theme => ({
+  headerMenuItem: {
+    margin: 20,
+    padding: 10,
+    textAlign: 'left'
+  },
+  headerMenuItemTitle: {
+    cursor: 'pointer',
+    '&:hover': {
+      color: theme.palette.primary.main
+    }
+  }
+})
+
+class Home extends Component {
+  handleClickMenuItem(url) {
+    const { history } = this.props
+    history.push(url)
+  }
+
+  menuBoard() {
+    const { classes } = this.props
+    return (
+      <div>
+        {MenuConfig.slice(1).map(menuItem => (
+          <div key={menuItem.name} className={classes.headerMenuItem}>
+            <Typography
+              variant="subtitle1"
+              className={classes.headerMenuItemTitle}
+              onClick={_ => this.handleClickMenuItem(menuItem.path)}
+            >
+              {menuItem.title}
+            </Typography>
+            <Typography variant="subtitle2" style={{ color: '#aaa' }}>
+              {menuItem.desc}
+            </Typography>
+          </div>
+        ))}
+      </div>
+    )
+  }
+
   render() {
-    return <div>Home</div>
+    return <div>{this.menuBoard()}</div>
   }
 }
+
+export default withStyles(styles)(withRouter(Home))
