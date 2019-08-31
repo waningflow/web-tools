6ecdfb97af427055eb65eb2d4595c34df849bb9e
waningflow <jack1995woods@gmail.com>
2019-08-31 22:43:54 +0800

    update json parser

--- src/pages/JsonParser/JsonParser.js
+++ src/pages/JsonParser/JsonParser.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react'
+import TextField from '@material-ui/core/TextField'
+import ReactJson from 'react-json-view'
+import './JsonParser.css'
 
 export default class JsonParser extends Component {
+  state = {
+    inputStr: ''
+  }
+
+  parseJson = str => {
+    let rtn = undefined
+    try {
+      rtn = JSON.parse(str)
+    } catch (e) {
+      console.error(str)
+    }
+    return rtn
+  }
+
+  handleChangeInput = e => {
+    this.setState({
+      inputStr: e.target.value
+    })
+  }
+
   render() {
-    return <div>
-      json parser
-    </div>
+    const { inputStr } = this.state
+    const inputJson = this.parseJson(inputStr)
+    return (
+      <div className="jsonParserContainer">
+        <div className="jsonParserInputContainer">
+          <TextField
+            fullWidth
+            multiline
+            variant="outlined"
+            rows="5"
+            onChange={this.handleChangeInput}
+            placeholder="Paste json string here"
+          />
+        </div>
+        <div className="jsonParserOutputContainer">
+          <ReactJson
+            src={inputJson}
+            theme="monokai"
+            displayDataTypes={false}
+            style={{ padding: '6px', borderRadius: '3px', height: window.innerHeight - 300 }}
+          />
+        </div>
+      </div>
+    )
   }
 }
